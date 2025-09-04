73a681f65359c4181ffc006290377e9ee85e42a9
Mikolaj Klikowicz <mklikowicz@antmicro.com>
2025-09-04 14:57:46 +0200

    [#83049] Fix incorrect version
    Signed-off-by: Mikolaj Klikowicz <mklikowicz@antmicro.com>

--- test/test_import.py
+++ test/test_import.py
@@ -9,7 +9,7 @@ import pytest
 def test_import():
     import bt2
 
-    assert bt2.__version__ == "2.2.0rc1"
+    assert bt2.__version__ == "0.0.1"
 
     with pytest.raises(RuntimeError) as exc:
         bt2.TraceCollectionMessageIterator("/this/path/does/not/exist")
